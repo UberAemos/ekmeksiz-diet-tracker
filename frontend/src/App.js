48bf147b11c157c0397c60ff66663b47e4eaf7f1
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2019-04-12 18:09:51 +0300

    Added react-route-dom, formik, moment. Added SignUpComponent and made the required routing actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.css"
+import Ekmeksiz from './component/Ekmeksiz'
 
 class App extends Component {
   render() {
-    return <div className="App">Hello from react</div>;
+    return (
+      <div className="App">
+        <Ekmeksiz />
+      </div>
+    )
   }
 }
 
---
+++ frontend/src/component/SignUpComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignUpComponent extends Component {
+  render() {
+    return (
+      <div>
+        Hi from SignUpComponent
+      </div>
+    )
+  }
+}
