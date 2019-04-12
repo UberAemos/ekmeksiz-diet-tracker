8c788ed17d1a38bf944267bd7c599f32c7b9a524
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2019-04-12 15:32:27 +0300

    Initial commit

---
+++ backend/src/main/java/com/uberaemos/ekmeksizdiettracker/EkmeksizDietTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.uberaemos.ekmeksizdiettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkmeksizDietTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkmeksizDietTrackerApplication.class, args);
+	}
+
+}
