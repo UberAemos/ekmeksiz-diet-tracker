ca3cfc3250dba0f591923271d1c68c8fe58ed24e
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2019-04-12 18:31:44 +0300

    Added bootstrap and made simple signup page

--- frontend/src/component/SignUpComponent.jsx
+++ frontend/src/component/SignUpComponent.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import {Formik, Field, Form} from 'formik'
 
 export default class SignUpComponent extends Component {
   render() {
     return (
       <div>
-        Hi from SignUpComponent
+        <Formik>
+          <Form className="form-signup w-25 mx-auto">
+          <h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
+          <fieldset className="form-group">
+          <Field className="form-control" type="text" name="name" placeholder="Name" />
+          </fieldset>
+          <fieldset className="form-group">
+          <Field className="form-control" type="text" name="surname" placeholder="Surname" />
+          </fieldset>
+          <fieldset className="form-group">
+          <Field className="form-control" type="text" name="username" placeholder="Username" />
+          </fieldset>
+          <fieldset className="form-group">
+          <Field className="form-control" type="text" name="password" placeholder="Password" />
+          </fieldset>
+          <button type="submit" className="btn btn-success">Sign Up</button>
+          </Form>
+        </Formik>
       </div>
     )
   }
