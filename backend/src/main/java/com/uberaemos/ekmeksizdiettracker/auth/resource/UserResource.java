9926339a9b41c35a59556a99abb9048706423f5e
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2019-04-17 00:57:48 +0300

    Password encoding is added

--- backend/src/main/java/com/uberaemos/ekmeksizdiettracker/auth/resource/UserResource.java
+++ backend/src/main/java/com/uberaemos/ekmeksizdiettracker/auth/resource/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +23,14 @@ public class UserResource {
 	@Autowired
 	private UserRepository userRepository;
 	
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
 	@PostMapping("/register")
 	public ResponseEntity<User> registerUser(
 			@RequestBody User user) {
 		
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		User createdUser = userRepository.save(user);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
